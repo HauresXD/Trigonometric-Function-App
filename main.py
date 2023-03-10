0c69d4f7b0e22b1e15295f517e052f04b4b50610
Haures <markvart155@seznam.cz>
2023-03-10 22:43:40 +0100

    main.py frames

--- main.py
+++ main.py
@@ -12,12 +12,22 @@ class Application(tk.Tk):
         super().__init__(className=self.name)
         self.title(self.name)
         self.bind("<Escape>", self.quit)
-        self.geometry("800x600")
+        self.geometry("1300x800")
+        self.resizable(width=tk.FALSE, height=tk.FALSE)
         if platform.system() == "Windows":
             self.iconbitmap("images/icon.ico")
 
-        self.headerFrame = tk.Frame(self).pack()
-        self.headerLabel = tk.Label(self.headerFrame, text="Goniometrické funkce", font=("Arial", 24)).pack()
+        self.headerFrame = tk.LabelFrame(self, borderwidth = 0, highlightthickness = 0)
+        self.headerFrame.pack(side=tk.TOP)
+        self.headerLabel = tk.Label(self.headerFrame, text="Goniometrické funkce", font=("Arial", 24))
+        self.headerLabel.pack()
+
+        self.contentFrame = tk.Frame(self, bg="blue", width=1300, height=800)
+        self.contentFrame.pack(side=tk.TOP)
+        self.entryFrame = tk.Frame(self.contentFrame, bg="#c3c3c3", width=700, height=800)
+        self.entryFrame.pack(side=tk.LEFT)
+        self.graphFrame = tk.Frame(self.contentFrame, bg="#aeaeae", width=600, height=800)
+        self.graphFrame.pack(side=tk.RIGHT)
         
 
     def quit(self, event=None):
