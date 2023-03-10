8aacd3bc30336e22d15cd3c976f6165b98346558
Patrik Markvart <mar39178@spseol.cz>
2023-03-10 13:43:50 +0100

    edit main.py + added icon + restructureing

--- main.py
+++ main.py
@@ -4,39 +4,6 @@ from os.path import basename, splitext
 import tkinter as tk
 # from matplotlib import pyplot as plt
 
-# from tkinter import ttk
-
-class MyEntry(tk.Entry):
-    def __init__(self, master=None, cnf={}, **kw):
-        super().__init__(master, cnf, **kw)
-
-        if not "textvariable" in kw:
-            self.variable = tk.StringVar()
-            self.config(textvariable=self.variable)
-        else:
-            self.variable = kw["textvariable"]
-
-    @property
-    def value(self):
-        return self.variable.get()
-
-    @value.setter
-    def value(self, new: str):
-        self.variable.set(new)
-
-
-class About(tk.Toplevel):
-    def __init__(self, parent):
-        super().__init__(parent, class_=parent.name)
-        self.config()
-
-        btn = tk.Button(self, text="Konec", command=self.close)
-        btn.pack()
-
-    def close(self):
-        self.destroy()
-
-
 class Application(tk.Tk):
     name = basename(splitext(basename(__file__.capitalize()))[0])
     name = "Goniometrické funkce"
@@ -45,6 +12,8 @@ class Application(tk.Tk):
         super().__init__(className=self.name)
         self.title(self.name)
         self.bind("<Escape>", self.quit)
+        self.geometry("800x600")
+        self.iconbitmap("images/icon.ico")
 
         
 
